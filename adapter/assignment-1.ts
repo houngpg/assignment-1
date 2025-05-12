599e3597ca5400bbb4c1f25379c98a4e7436d498
houngpg <houngpg@mcmaster.ca>
2025-05-12 01:07:58 +0000

    updated assignment-1.ts

--- adapter/assignment-1.ts
+++ adapter/assignment-1.ts
@@ -9,18 +9,18 @@ export interface Book {
 };
 
 // If you have multiple filters, a book matching any of them is a match.
-async function listBooks(filters?: Array<{from?: number, to?: number}>) : Promise<Book[]>{
-    // if (!filters || filters.length === 0) {
-    //     return books; // No filters, return all books
-    // }
-    // console.log("running listBooks")
-    // return books.filter(book =>
-    //     filters.some(filter =>
-    //         (filter.from === undefined || book.price >= filter.from) &&
-    //         (filter.to === undefined || book.price <= filter.to)
-    //     )
-    // );
-    throw new Error("Todo")
+async function listBooks(filters?: Array<{ from?: number, to?: number }>): Promise<Book[]> {
+    if (!filters || filters.length === 0) {
+        return books; // No filters, return all books
+    }
+    console.log("running listBooks")
+    return books.filter(book =>
+        filters.some(filter =>
+            (filter.from === undefined || book.price >= filter.from) &&
+            (filter.to === undefined || book.price <= filter.to)
+        )
+    );
+    // throw new Error("Todo")
 }
 
 const assignment = "assignment-1";
