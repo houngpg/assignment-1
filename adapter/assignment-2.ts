fd145606785d0ac291b4bd93821cfbd4a6c0bcc4
houngpg <houngpg@mcmaster.ca>
2025-05-18 20:15:15 +0000

    Added mongoDB to docker-compose, updated adapter connection, updated book.rest to include test requests, added functions for creating/updating and deleting books

--- adapter/assignment-2.ts
+++ adapter/assignment-2.ts
@@ -11,17 +11,40 @@ export interface Book {
     image: string,
 };
 
-async function listBooks(filters?: Array<{from?: number, to?: number}>) : Promise<Book[]>{
+async function listBooks(filters?: Array<{ from?: number, to?: number }>): Promise<Book[]> {
     return assignment1.listBooks(filters);
-}
+};
 
 async function createOrUpdateBook(book: Book): Promise<BookID> {
-    throw new Error("Todo")
-}
+    // API request for creating or updating a book
+    let result = await fetch('http://localhost:3000/books', {
+        method: "POST",
+        body: JSON.stringify(book),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    // Response handling for the request
+    if (result.ok) {
+        let res = await result.json() as { id: BookID };
+        return res.id;
+    }
+    // Error handling
+    else {
+        throw new Error("Failed to create/update book.")
+    }
+};
 
 async function removeBook(book: BookID): Promise<void> {
-    throw new Error("Todo")
-}
+    // API request for deleting a book
+    let result = await fetch('http://localhost:3000/books/${book}', {
+        method: "DELETE"
+    });
+    // Error handling for the request (Error)
+    if (!result.ok) {
+        throw new Error("Failed to delete book.")
+    }
+};
 
 const assignment = "assignment-2";
 
