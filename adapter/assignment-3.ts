7dc7ffcd35015d577d32998c5ed49bddb263cbbc
houngpg <houngpg@users.noreply.github.com>
2025-05-25 22:53:18 +0000

    Prettified Code!

--- adapter/assignment-3.ts
+++ adapter/assignment-3.ts
@@ -3,78 +3,81 @@ import previous_assignment from "./assignment-2";
 export type BookID = string;
 
 export interface Book {
-    id?: BookID;
-    name: string;
-    author: string;
-    description: string;
-    price: number;
-    image: string;
+  id?: BookID;
+  name: string;
+  author: string;
+  description: string;
+  price: number;
+  image: string;
 }
 
 export interface Filter {
-    from?: number;
-    to?: number;
-    name?: string;
-    author?: string;
+  from?: number;
+  to?: number;
+  name?: string;
+  author?: string;
 }
 
 // If multiple filters are provided, any book that matches at least one of them should be returned
 // Within a single filter, a book would need to match all the given conditions
 async function listBooks(filters?: Filter[]): Promise<Book[]> {
-    let result;
-    if (!filters || filters.length === 0) {
-        result = await fetch(`http://localhost:3000/books`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            }
-        })
-        return await result.json() as Book[]
+  let result;
+  if (!filters || filters.length === 0) {
+    result = await fetch(`http://localhost:3000/books`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return (await result.json()) as Book[];
+  }
+
+  // Filters through each filters then joins as queryString
+  let queryArray = [] as Array<string>;
+  let count = 0;
+  for (const filter of filters) {
+    if (filter.from) {
+      queryArray = [...queryArray, `filters[${count}][from]=${filter.from}`];
     }
-
-    // Filters through each filters then joins as queryString
-    let queryArray = [] as Array<string>
-    let count = 0
-    for (const filter of filters) {
-        if (filter.from) {
-            queryArray = [...queryArray, `filters[${count}][from]=${filter.from}`]
-        }
-        if (filter.to) {
-            queryArray = [...queryArray, `filters[${count}][to]=${filter.to}`]
-        }
-        if (filter.author) {
-            queryArray = [...queryArray, `filters[${count}][author]=${filter.author}`]
-        }
-        if (filter.name) {
-            queryArray = [...queryArray, `filters[${count}][name]=${filter.name}`]
-        }
-        count++
+    if (filter.to) {
+      queryArray = [...queryArray, `filters[${count}][to]=${filter.to}`];
     }
-    const queryString = queryArray.join("&")
+    if (filter.author) {
+      queryArray = [
+        ...queryArray,
+        `filters[${count}][author]=${filter.author}`,
+      ];
+    }
+    if (filter.name) {
+      queryArray = [...queryArray, `filters[${count}][name]=${filter.name}`];
+    }
+    count++;
+  }
+  const queryString = queryArray.join("&");
 
-    result = await fetch(`http://localhost:3000/books?${queryString}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        }
-    })
+  result = await fetch(`http://localhost:3000/books?${queryString}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    return await result.json() as Book[]
+  return (await result.json()) as Book[];
 }
 
 async function createOrUpdateBook(book: Book): Promise<BookID> {
-    return await previous_assignment.createOrUpdateBook(book);
+  return await previous_assignment.createOrUpdateBook(book);
 }
 
 async function removeBook(book: BookID): Promise<void> {
-    await previous_assignment.removeBook(book);
+  await previous_assignment.removeBook(book);
 }
 
 const assignment = "assignment-3";
 
 export default {
-    assignment,
-    createOrUpdateBook,
-    removeBook,
-    listBooks,
+  assignment,
+  createOrUpdateBook,
+  removeBook,
+  listBooks,
 };
--- src/routes.ts
+++ src/routes.ts
@@ -18,7 +18,12 @@ export interface Book {
 
 router.get("/books", async (ctx) => {
   // Added querying for author and book title (name)
-  const filters = ctx.query.filters as Array<{ from?: string; to?: string; author?: string; name?: string }>;
+  const filters = ctx.query.filters as Array<{
+    from?: string;
+    to?: string;
+    author?: string;
+    name?: string;
+  }>;
 
   // Added validation to filters
   if (!validateFilters(filters)) {
@@ -29,53 +34,57 @@ router.get("/books", async (ctx) => {
   try {
     await client.connect();
 
-    let books = [] as Array<Book>
+    let books = [] as Array<Book>;
     if (!filters || filters.length === 0) {
-      ctx.body = await collection.find({}).toArray()
+      ctx.body = await collection.find({}).toArray();
       return;
     }
 
     for (const filter of filters) {
-      const query = {} as { price: { $gte?: number, $lte?: number }, author: string, name: string }
-      const price = {} as { $gte?: number, $lte?: number }
+      const query = {} as {
+        price: { $gte?: number; $lte?: number };
+        author: string;
+        name: string;
+      };
+      const price = {} as { $gte?: number; $lte?: number };
       // Checks if the price from and to are valid, then adds it to the query
       if (filter.from) {
-        price["$gte"] = parseFloat(filter.from)
+        price["$gte"] = parseFloat(filter.from);
       }
       if (filter.to) {
-        price["$lte"] = parseFloat(filter.to)
+        price["$lte"] = parseFloat(filter.to);
       }
       if (Object.keys(price).length !== 0) {
-        query.price = price
+        query.price = price;
       }
       // Adds author and name to query
       if (filter.author) {
-        query.author = filter.author
+        query.author = filter.author;
       }
       if (filter.name) {
-        query.name = filter.name
+        query.name = filter.name;
       }
-      console.log(query)
+      console.log(query);
 
-      const result = await collection.find(query).map(
-        doc => {
+      const result = await collection
+        .find(query)
+        .map((doc) => {
           const book: Book = {
             id: doc._id.toHexString(),
             name: doc.name,
             image: doc.image,
             price: doc.price,
             author: doc.author,
-            description: doc.description
-          }
-          return book
-        }
-      ).toArray()
-      books = [...books, ...result]
+            description: doc.description,
+          };
+          return book;
+        })
+        .toArray();
+      books = [...books, ...result];
     }
 
     await client.close();
-    ctx.body = books
-
+    ctx.body = books;
   } catch (error) {
     ctx.status = 500;
     ctx.body = { error: `Failed to fetch books due to: ${error}` };
