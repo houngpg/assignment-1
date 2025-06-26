94857735b6be189e41aefc39e3f318311c02e145
houngpg <houngpg@mcmaster.ca>
2025-06-26 01:01:43 +0000

    Added SDK generation, updated vitest, updated CI workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
+import vitestOpenapiPlugin from './vitest-openapi-plugin'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-
+        exclude: ["./build", "./client"]
     },
+    plugins: [
+        {
+            ...vitestOpenapiPlugin
+        }
+    ]
 })
